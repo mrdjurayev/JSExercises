a67b22e0699919fd2511802079d0f0d3be3631bf
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-21 17:30:46 +0500

    Exercise 3 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,3 +1,9 @@
-// 2. Write a Javascript program to print the current window contents
-const btn = document.getElementById('print-screen');
-btn.addEventListener('click', () => window.print());
+// 3. Write a Javascript program to get the current date. Expected output: mm-dd-yyyy
+const dateObject = new Date();
+
+const month = dateObject.getMonth() + 1;
+const today = dateObject.getDate();
+const year = dateObject.getFullYear();
+
+const displayDate = document.getElementById('current-date');
+displayDate.textContent = `${month}-${today}-${year}`;
