b49339a970270f7e90222f2b6035089984cba4c1
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-21 17:08:42 +0500

    Exercise 2 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,13 +1,3 @@
-// 1. Write a javascript program to display the current day and time in the following format. Sample output: Today is : Tuesday, current time is: 10PM : 30 : 38;
-const date = new Date();
-const day = date.getDay();
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const currentDate = document.getElementById('current-date');
-currentDate.textContent = `Today is: ${days[day]}`;
-
-const hour = String(date.getHours()).padStart(2, '0');
-const minut = String(date.getMinutes()).padStart(2, '0');
-const second = String(date.getSeconds()).padStart(2, '0');
-
-const currentTime = document.getElementById('current-time');
-currentTime.textContent = `Current time: ${hour}:${minut}:${second}`;
+// 2. Write a Javascript program to print the current window contents
+const btn = document.getElementById('print-screen');
+btn.addEventListener('click', () => window.print());
