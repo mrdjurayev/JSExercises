8a305b8d5a10acca8baef3f79d89bec99dd5161a
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-23 21:27:46 +0500

    Exercise 7 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,8 +1,7 @@
-// 6. Write a Javascript program to determine whether a given year is leap year in the Gregorian Calendar
-function leapYear(year) {
-    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+// 7. Write a Javascript program to find out if 1st january will be a Sunday berween 2014 and 2050
+for(let year = 2014; year <= 2050; year++) {
+    const date = new Date(year, 0, 1);
+    if(date.getDay() === 0) {
+        console.log(`1st January ${year} is a Sunday`);
+    }
 }
-
-console.log(leapYear(2016));
-console.log(leapYear(2000));
-console.log(leapYear(2001));
