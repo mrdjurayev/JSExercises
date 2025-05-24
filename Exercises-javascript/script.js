ec2f8bd638eab8cabea0ba66576eb8092e55f68b
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-24 17:30:36 +0500

    Exercise 10 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,10 +1,29 @@
-// 8. Write a Javascript program where the prgram takes a random integer between 1 and 10 ,
-// and the user is then prompted to input a guess number. The program displays a message "Good work" 
-// if the input matches the guess number otherwise "not mached".
-let randomNumber = Math.floor((Math.random() * 10)) + 1;
-let inputNumber = prompt('Enter your number');
-if(Number(inputNumber) === randomNumber) {
-    alert("Good work");
-} else {
-    alert("incorrect");
+// 10. Write a Javascript program to calculate multiplication and division of two numbers (input from the user)
+function calculator() {
+    const result = document.getElementById('result');
+
+    function getNumbers() {
+        const number1 = parseFloat(document.getElementById('number1').value);
+        const number2 = parseFloat(document.getElementById('number2').value);
+        return {number1, number2};
+    }
+
+    document.getElementById('division').addEventListener('click', () => {
+        const {number1, number2} = getNumbers();
+        if(number2 === 0) {
+            result.textContent = `Division by zero is not allowed!`;
+        } else {
+            result.textContent = `The result is: ${number1 / number2}`;
+        }        
+    });
+
+    document.getElementById('multiplication').addEventListener('click', ()  => {
+        const {number1, number2} = getNumbers();
+        result.textContent = `The result is: ${number1 * number2}`;
+    });
+    
+
 }
+
+calculator();
+
