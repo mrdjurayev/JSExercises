7ece299ea920e240751fca92006980401acdb6eb
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-25 14:04:21 +0500

    Exercise 13 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,3 +1,5 @@
-// 12. Write a Javascript program to get the website URL (loasing page)
-console.log(document.URL); // or
-// alert(document.URL);
+const name = prompt("Enter your variable for name"); //And the variable is created like this.
+const age = 20;
+this[name] = age;
+
+console.log(myAge); // Display the entered result in the console using its name.
