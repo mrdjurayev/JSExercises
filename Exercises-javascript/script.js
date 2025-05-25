11f3ed7ca15b77af31d3b40d308ae0dfb2c0d264
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-25 19:13:50 +0500

    Exercise 15 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,6 +1,10 @@
-// 14. Write a Javascript program to get the filename extension
-function getFileExtension(filename) {
-    return filename.split('.').pop(); //it splits the filename by the dot and converts in into an array and pop takes the last element
+// 15. Write a Javascript program to get the difference between a given number and 13, if the number is broader than 13 return double the absolute difference
+function difference13(n) {
+    if(n <= 13) {
+        return 13 - n;
+    } else {
+        return 2 * (n - 13);
+    }
 }
 
-console.log(getFileExtension('Javascript.pdf')); // pdf
+console.log(difference13(12));
