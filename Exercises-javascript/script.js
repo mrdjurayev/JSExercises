9b34d0670e67e93fb48ac78d24112682f1621db4
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-25 13:44:05 +0500

    Exercise 12 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,14 +1,3 @@
-function calculateCelsius() {
-    const input = parseFloat(document.getElementById('celsius').value);
-    const f = document.getElementById('result-celsius');
-    f.textContent = input * 9 / 5 + 32;
-}
-
-function calculateFarenheit() {
-    const input = parseFloat(document.getElementById('farenheit').value);
-    const c = document.getElementById('result-farenheit');
-    c.textContent = (input - 32) / 9 * 5;
-}
-
-document.getElementById('convert-celsius').addEventListener('click', calculateCelsius);
-document.getElementById('convert-farenheit').addEventListener('click', calculateFarenheit);
+// 12. Write a Javascript program to get the website URL (loasing page)
+console.log(document.URL); // or
+// alert(document.URL);
