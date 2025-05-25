aed4a28bf057603ba15d5e2f07c3f803449849bb
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-25 14:15:29 +0500

    Exercise 14 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,5 +1,6 @@
-const name = prompt("Enter your variable for name"); //And the variable is created like this.
-const age = 20;
-this[name] = age;
+// 14. Write a Javascript program to get the filename extension
+function getFileExtension(filename) {
+    return filename.split('.').pop(); //it splits the filename by the dot and converts in into an array and pop takes the last element
+}
 
-console.log(myAge); // Display the entered result in the console using its name.
+console.log(getFileExtension('Javascript.pdf')); // pdf
