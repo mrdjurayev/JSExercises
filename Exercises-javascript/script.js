78405d94e33931ecbfdfb1f2e4ca66b436361344
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-26 11:15:56 +0500

    Exercise 22 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,14 +1,8 @@
-// 21. Write a Javascript program to create another string  by addding "Py" in front of a given string. If the given string begins with "Py" return the original string
-function addPy(str) {
-    if(str.startsWith("Py")) {
-        return str;
-    } else {
-        return "Py" + str;
-    }
+// 22. Write a Javascript program to  remove a character at the specified position in a given string and return the modified string
+const removeCharacter = (str, position) => {
+    const part1 = str.substring(0, position); // [ 0 - position ) it  takes and creates a new string
+    const part2 = str.substring(position + 1, str.length); // [ position + 1 - to the end ] it takes the rest, excluding the position
+    return part1 + part2;
 }
 
-console.log(addPy("autoGUI"));
-
-// short version: 
-const addPy = str => str.startsWith("Py") ? str : "Py" + str;
-console.log(addPy('hello')); // same result as the below code, just comment out the previous code
+console.log(removeCharacter('Javascript', 5));
