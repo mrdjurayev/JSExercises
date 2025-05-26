9dd7bb728fff059575ed84cb0ee0f3aab2f54b8d
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-26 06:35:29 +0500

    Exercise 21 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,10 +1,14 @@
-// 20. Write a Javascript program to check two given integers whether one is positive and another one is negative
-function positiveNegative(a, b) {
-    return (a > 0 && b < 0) || (a < 0 && b > 0);
+// 21. Write a Javascript program to create another string  by addding "Py" in front of a given string. If the given string begins with "Py" return the original string
+function addPy(str) {
+    if(str.startsWith("Py")) {
+        return str;
+    } else {
+        return "Py" + str;
+    }
 }
 
-console.log(positiveNegative(5, -3));  // true
-console.log(positiveNegative(-7, 9));  // true
-console.log(positiveNegative(0, -1));  // false
-console.log(positiveNegative(4, 6));   // false
-console.log(positiveNegative(-2, -8)); // false
+console.log(addPy("autoGUI"));
+
+// short version: 
+const addPy = str => str.startsWith("Py") ? str : "Py" + str;
+console.log(addPy('hello')); // same result as the below code, just comment out the previous code
