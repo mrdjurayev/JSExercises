3ae482f05a948fc01057a696b24e1c50a623f691
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-27 19:30:22 +0500

    Exercise 37  solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,6 +1,12 @@
-// 36. Write a Javascript program that checks whether the last digit of three positive integers in the same.
-function sameThreeNumbers(a, b, c) {
-    return a % 10 === b % 10 && b % 10 === c % 10;
+//  37. Write a Javascript program produce a new string that has the first 3 characters in lower case from a given 
+// string. If the  string length is  less than 3 convert characters to  upper case
+function modifyString(str) {
+    if(str.length < 3) return str.toUpperCase();
+    else {
+        const part1 = str.substring(0, 3);
+        const part2 = str.substring(3);
+        return part1.toLowerCase() + part2;
+    }
 }
 
-console.log(sameThreeNumbers(12, 1, 3412));
+console.log(modifyString('Hi'));
