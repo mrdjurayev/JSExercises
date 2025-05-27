751efe0534849d086eb8aa3426944049bfebedba
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-27 11:37:22 +0500

    Exercise 32 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,10 +1,8 @@
-// 30. Write a JavaScript program to check whether a string "Script" appears at the 5th (index 4) position in
-//  a given string. If "Script" appears in the string, return the string without 
-// "Script" otherwise return the original one.  
-
-function checkScript(str) {
-    const startScript = str.substring(4, 10);
-    return startScript === 'Script' ? str.substring(0, 4) : str; 
+// 32. Write a Javascript program to find the largest of three given integers
+function findLargestIntegers(a, b, c) {
+    if(a >= b && a >= c) return a;
+    else if(b >= a && b >= c) return b;
+    else return c;
 }
 
-console.log(checkScript('wellScript'));
+console.log(findLargestIntegers(10, 15, 15));
