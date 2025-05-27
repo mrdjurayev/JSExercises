daf63b6ef58a4a55ca30e83930a0d9abde7dcab3
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-27 13:27:55 +0500

    Exercise 35 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,7 +1,7 @@
-// 34. Write a Javascrip program to find the largest number from the two given positive integers. The two numbers are in the range 40..60 inclusive
-function findLargestRange(a, b) {
-    if(!(a >= 40 && a <= 60) || !(b >= 40 && b <= 60)) return `Invalid input: both numbers must be between 40 and 60 inclusive`;
-    else return Math.max(a, b);
+// 35. Write a  program to check  whether a specified character exists between the 2nd and 4th positions in a given string.
+function checkCharacter(str, char) {
+    const strBetween = str.substring(1, 4);
+    return strBetween.includes(char);
 }
 
-console.log(findLargestRange(5, 56));
+console.log(checkCharacter('hel', 'l'));
