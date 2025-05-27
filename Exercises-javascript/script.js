592c67f1fe9e9de457bee27b35e4ccc0303fe300
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-27 13:12:16 +0500

    Exercise 34 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,6 +1,7 @@
-// 33. Write a Javascript program to check whether two numbers are in the range 40..60 or 70..100 inclusive
-function checkRange(num1, num2) {
-    return (num1 >= 40 && num1 <= 60 || num1 >= 70 && num1 <= 100) && (num2 >= 40 && num2 <= 60 || num2 >= 70 && num2 <= 100)
+// 34. Write a Javascrip program to find the largest number from the two given positive integers. The two numbers are in the range 40..60 inclusive
+function findLargestRange(a, b) {
+    if(!(a >= 40 && a <= 60) || !(b >= 40 && b <= 60)) return `Invalid input: both numbers must be between 40 and 60 inclusive`;
+    else return Math.max(a, b);
 }
 
-console.log(checkRange(12, 23));
+console.log(findLargestRange(5, 56));
