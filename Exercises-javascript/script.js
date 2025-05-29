bf07b6c3e45bd04ca063ce1adccb64c7f3dee372
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-29 06:23:50 +0500

    Exercise 49 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,6 +1,20 @@
-// 48. Write a Javascript code to reverse a given string
-function reverseString(str) {
-    return str.split('').reverse().join('');
-} 
+// 49. Write a Javascript program to replace  every character in a given string with the character following in the alphabet
 
-console.log(reverseString('hello'));
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
+    , 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+function replaceCharacter(str) {
+    let letters = str.split('');
+    const replaceLetters = letters.map(letter => {
+        const index = alphabet.findIndex(let => let === letter);
+        return index === -1 || index === 25 ? 'a' : alphabet[index + 1];
+    });
+
+    return replaceLetters.join('');
+    
+}
+
+console.log(replaceCharacter('hello.'));
+
+
+
