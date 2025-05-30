aeca6b72a572ffb5b2946f40d2044fdf99cc7f57
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-30 22:39:12 +0500

    Exercise 50 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,20 +1,8 @@
-// 49. Write a Javascript program to replace  every character in a given string with the character following in the alphabet
+// 50. Write a Javascript program to capitalize the first letter of each word in a given string
 
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
-    , 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-];
-function replaceCharacter(str) {
-    let letters = str.split('');
-    const replaceLetters = letters.map(letter => {
-        const index = alphabet.findIndex(let => let === letter);
-        return index === -1 || index === 25 ? 'a' : alphabet[index + 1];
-    });
-
-    return replaceLetters.join('');
-    
+function capitalizeWords(text) {
+    const words = text.split(' ');
+    return words.map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
 }
 
-console.log(replaceCharacter('hello.'));
-
-
-
+console.log(capitalizeWords('hello world!'));
