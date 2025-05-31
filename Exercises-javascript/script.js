76ff669b8954c00d62b1d644dce4570ef0e2992a
mrdjurayev <mrabudjurayev@gmail.com>
2025-05-31 19:22:17 +0500

    Exercise 53 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,8 +1,15 @@
-// 52. Write a Javascript program to convert letters of a given string alphabetically
+// 53. Write a Javascript program to check whether the characters a and b are separated by exactly 3 places anywhere (at least once) in a given string
 
-function sortLetters(str) {
+function checkLetter(str) {
     const letters = str.split('');
-    return letters.sort().join('');
+    for(let i = 0; i < letters.length - 4; i++) {
+        if(
+            str[i] === 'a' && str[i + 4] === 'b' ||
+            str[i] === 'b' && str[i + 4] === 'a'
+        ) return true;        
+    }
+
+    return false;
 }
 
-console.log(sortLetters('hello'));
+console.log(checkLetter('askdb'));
