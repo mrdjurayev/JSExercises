615011c5a046a501c81b6cb9f4453f47f0dd6122
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-01 15:20:02 +0500

    Exercise 56 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,11 +1,13 @@
-// 55. Write a Javascript program to check whether  a given string contains an equal number of p's and t's
+// 56. Write a Javascript program to divide two positive numbers and return the result as string with properly formatted
 
-const countLetters = str => {
-    const letters = str.toLowerCase().split('');
-    const countP = letters.filter(letter => letter === 'p').length;
-    const countT = letters.filter(letter => letter === 't').length;
-    const isEqual = countP === countT;
-    return {countP, countT, isEqual};
+function divideAndFormat(a, b) {
+    if(a <= 0 || b <= 0) {
+        return `Please enter positive numbers!`;
+    }
+
+    const result = a / b;
+    return result.toLocaleString('en-US');
 }
 
-console.log(countLetters('hello Master'));
+console.log(divideAndFormat(1232, 900));
+
