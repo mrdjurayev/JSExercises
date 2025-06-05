0e0403fba98e7865f902fc4618fb83239c2ceb19
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-05 11:44:55 +0500

    Exercise 67 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,8 +1,18 @@
-// 66. Write a Javascript program to display the city name if the strings begins with "Los" or "New" otherwise return blank
+// 67. Write a Javascript program to create a new  string from a given string. This program removes the first and last characters of the string if the first and last character is 'P'. Return the original string if the condition is not satisfied.
 
-function displayCity(cityName) {
-    if(cityName.startsWith('Los') || cityName.startsWith('New')) return cityName;
-    return '';
+function removeCharacter(str) {
+    const firstChar = str.startsWith('P');
+    const lastChar = str.endsWith('P');
+
+    if(firstChar && lastChar) {
+        return str.slice(1, -1);
+    } else if(firstChar) {
+        return str.slice(1);
+    } else if(lastChar) {
+        return str.slice(0, -1);
+    }
+
+    return str;
 }
 
-console.log(displayCity('New-York'));
+console.log(removeCharacter('Python'));
