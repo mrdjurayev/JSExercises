325409b0248beb0c45504f5326a84243e11f9f41
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-07 13:35:41 +0500

    Exercise 78 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,7 +1,6 @@
-// 77. Write a Javascript program to test whether an array of integers of length 2 contains 1 or 3.
-
+// 78. Write a JavaScript program to test whether an array of integers of length 2 does not contain 1 or 3.  
 function checkNumber(arr) {
-    return arr.includes(1) || arr.includes(3);
+    return !arr.includes(1) && !arr.includes(3);
 }
 
-console.log(checkNumber([1, 2]));
+console.log(checkNumber([0, 2]));
