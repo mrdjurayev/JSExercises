ae50fb305109f275d3f69aea058844f38b5300cd
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-07 17:45:31 +0500

    Exercise 79 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,6 +1,10 @@
-// 78. Write a JavaScript program to test whether an array of integers of length 2 does not contain 1 or 3.  
+// 79. Write a Javascript program to test whether a given array of integers cotains 30 and 40 twice. The array length should be 0, 1 or 2.
+
 function checkNumber(arr) {
-    return !arr.includes(1) && !arr.includes(3);
+    if(arr.length === 2) {
+        return (arr[0] === 30 && arr[1] === 30) || (arr[0] === 40 && arr[1] === 40);
+    }
+    return false;
 }
 
-console.log(checkNumber([0, 2]));
+console.log(checkNumber([30, 30]));
