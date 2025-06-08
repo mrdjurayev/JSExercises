7585f4acf20dfeec449ec1a6497ada8cbd818137
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-08 17:07:35 +0500

    Exercise 82 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,11 +1,18 @@
-// 81. Write a Javascript program to add two digits to a given  positive integer of length two.
+// 82. Write a Javascript program to add two positive integer witout carrying.
 
-function add(number)  {
-    if(number >= 10 && number <= 99) {
-        return Math.floor(number / 10) + (number % 10);
+function addWithoutCarrying(a, b) {
+    const longer = Math.max(a.toString().length, b.toString().length);
+    const strA = a.toString().padStart(longer, '0');
+    const strB = b.toString().padStart(longer, '0');
+    
+    let added = '';
+    for(let i = 0; i < longer; i++) {
+        added += ((+strA[i] + +strB[i]) % 10).toString();
     }
 
-    return `The number length isn't 2!`;
+    return Number(added);
 }
 
-console.log(add(99));
+console.log(addWithoutCarrying(12, 48));
+
+
