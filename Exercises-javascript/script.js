0ccbe7178f894ff734c2f913ba8ca41f7c675a2a
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-08 18:59:02 +0500

    Exercise 84 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,16 +1,30 @@
-// 83. Write a Javascript program to find the longest string from a given array of strings.
+// 84. Write a Javascript program to replace each character in a given string with the next in the English alphabet.
+//  Note: 'a' will be replace by 'b' or 'z' would be replaced by 'a'.
 
-function findLongestString(arr) {
-    if(arr.length === 0) return 'Empty array!';
-
-    let longest = arr[0]; 
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i].length > longest.length) {
-            longest = arr[i];
+function replaceCharacter(str) {
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const alphabetUp = alphabet.map(letter => letter.toUpperCase());
+    
+    const replacedStr = str.split('').map(letter => {
+        if(letter === "z") {
+            return "a";
+        } else if(letter === 'Z') {
+            return "A";
         }
-    }
 
-    return longest;
+        for(let i = 0; i < alphabet.length; i++) {
+            if(letter === alphabet[i]) {
+                return alphabet[i + 1];
+            } else if(letter === alphabetUp[i]) {
+                return alphabetUp[i + 1];
+            }
+
+        }
+
+        return letter;
+    });
+
+    return replacedStr.join('');
 }
 
-console.log(findLongestString(['hello', 'Javascript']));
+console.log(replaceCharacter('hello javascript'));
