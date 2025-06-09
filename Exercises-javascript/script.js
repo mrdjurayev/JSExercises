3b8993f1e99b2897dcd28e1831d6897fbacf6849
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-09 12:46:35 +0500

    Exercise 86 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,16 +1,20 @@
-// 85. Write a JavaScript program to divide a given array of positive integers into 
-// two parts. First element belongs to the first part, second element belongs to 
-// the second part, and third element belongs to the first part and so on. 
-// Now compute the sum of two parts and store it in an array of size two.
+//86. Write a Javascript program to find the types of a given angle.
 
-function splitAndSum(arr) {
-    const firstElements = arr.filter((num, index) => index % 2 === 0);
-    const secondElements = arr.filter((num, index) => index % 2 !== 0);
+// Acute angle: An angle between 0 and 90 degrees.
+// Right angle: An 90 degree angle.
+// Obtuse angle: An angle between 90 and 180 degrees.
+// Straight angle: A 180 degree angle.
 
-    const sum1 = firstElements.reduce((acc, num) => acc + num, 0);
-    const sum2 = secondElements.reduce((acc, num) => acc + num, 0);
-
-    return [sum1, sum2];
+function findNameAngle(deg) {
+    if(deg > 0 && deg < 90) {
+        return 'Acute angle';
+    } else if(deg === 90) {
+        return 'Right angle';
+    } else if(deg > 90 && deg < 180) {
+        return 'Obtuse angle';
+    } else if(deg === 180) {
+        return 'Straight angle';
+    } else return 'The degree is invalid for this example!';
 }
 
-console.log(splitAndSum([1, 2, 3, 4, 5]));
+console.log(findNameAngle(180));
