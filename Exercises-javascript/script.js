156cf9e7230cf527ff94d93ff5ca4c1056692f02
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-09 19:05:39 +0500

    Exercise 91 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,9 +1,16 @@
-// 90. Write a Javascript program to find the k th greatest element in a  given array  of integers.
+// 91. Write a Javascript program to find the maximum possible sum of some of its k consecutive numbers in a  given array of positive integers.
 
-function findGreatest(arr, k) {
-    const sorted = arr.sort((a, b) => b - a);
 
-    return sorted[k-1];
+let k = 3;
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+let separatedSumGroup = []; 
+
+for (let i = 0; i <= numbers.length - k; i++) {
+    const separated = numbers.slice(i, i + k);
+    const separatedSum = separated.reduce((acc, val) => acc + val, 0);
+    separatedSumGroup.push(separatedSum);
 }
 
-console.log(findGreatest([1,2, 232, 3444, 55], 2));
+console.log(Math.max(...separatedSumGroup)); 
+
+
