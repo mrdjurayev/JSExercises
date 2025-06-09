1681bf0ab9acf0815b917a4ca184a7281d839d4f
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-09 13:54:09 +0500

    Exercise 89 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,9 +1,15 @@
-// 88. Write a JavaScript program that takes two integers and a divisor. 
-// If the given divisor divides both integers and does not divide either, 
-// two specified integers are similar. Check whether two integers are similar or not.  
-
-function checkSimilarity(a, b, divisor) {
-    return (a % divisor !== 0 && b % divisor !== 0) || (a % divisor === 0 && b % divisor === 0);
+//89. Write a JavaScript program to check whether it is possible to replace $ in a given expression x $ y = z with 
+// one of the four signs +, -, * or / to obtain a correct expression.  
+// For example x = 10, y = 30 and z = 300, we can replace $ with a multiple operator (*) to obtain x * y = z
+ 
+const checkExpression = (x, y, z) => {
+    return (
+        x + y === z ||
+        x / y === z ||
+        x * y === z ||
+        x - y === z
+    );
 }
 
-console.log(checkSimilarity(16, 23, 3));
+console.log(checkExpression(10, 30, 300));
+
