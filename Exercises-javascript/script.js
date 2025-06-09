f5becc57f379a3d3c254dc2aeb90f4f5f797e1da
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-09 17:50:10 +0500

    Exercise 90 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,15 +1,9 @@
-//89. Write a JavaScript program to check whether it is possible to replace $ in a given expression x $ y = z with 
-// one of the four signs +, -, * or / to obtain a correct expression.  
-// For example x = 10, y = 30 and z = 300, we can replace $ with a multiple operator (*) to obtain x * y = z
- 
-const checkExpression = (x, y, z) => {
-    return (
-        x + y === z ||
-        x / y === z ||
-        x * y === z ||
-        x - y === z
-    );
+// 90. Write a Javascript program to find the k th greatest element in a  given array  of integers.
+
+function findGreatest(arr, k) {
+    const sorted = arr.sort((a, b) => b - a);
+
+    return sorted[k-1];
 }
 
-console.log(checkExpression(10, 30, 300));
-
+console.log(findGreatest([1,2, 232, 3444, 55], 2));
