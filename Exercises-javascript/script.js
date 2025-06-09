14ea0d1f5338e856c3553e6bbf369015ce59ca57
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-09 19:33:06 +0500

    Exercise 92 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,16 +1,10 @@
-// 91. Write a Javascript program to find the maximum possible sum of some of its k consecutive numbers in a  given array of positive integers.
+// 92. Write a Javascript program to find the maximum difference between any two adjacent  elements of a given array integers.
 
-
-let k = 3;
-const numbers = [1, 2, 3, 4, 5, 6, 7];
-let separatedSumGroup = []; 
-
-for (let i = 0; i <= numbers.length - k; i++) {
-    const separated = numbers.slice(i, i + k);
-    const separatedSum = separated.reduce((acc, val) => acc + val, 0);
-    separatedSumGroup.push(separatedSum);
+const numbers = [1, 20, 30, 49090, 5];
+let separateGroup = [];
+for(let i = 0; i < numbers.length - 1; i++) {
+    const diff = Math.abs(numbers[i + 1] - numbers[i]);
+    separateGroup.push(diff);
 }
 
-console.log(Math.max(...separatedSumGroup)); 
-
-
+console.log(Math.max(...separateGroup));
