72a5ccec7740e299cda7e7f569bd4622ff0edd20
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-10 17:25:19 +0500

    Exercise 94 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,10 +1,19 @@
-// 93. Write a JavaScript program to find the maximum difference among all possible pairs of a given array of integers. 
+// 94. Write a Javascript program to find the number appearing most frequently in a given array of integers.
 
-function maxDifference(arr) {
-    const max = Math.max(...arr);
-    const min = Math.min(...arr);
+function mostFrequent(arr) {
+    let count = {};
+    let maxCount = 0;
+    let result = null;
 
-    return max - min;
+    for(let num of arr) {
+        count[num] = (count[num] || 0) + 1;
+
+        if(count[num] > maxCount) {
+            maxCount = count[num];
+            result = num;
+        }
+    }
+    return result;
 }
 
-console.log(maxDifference([1, 2, 34, 55, 90]));
+console.log(mostFrequent([1, 2, 3, 4, 3, 6]));
