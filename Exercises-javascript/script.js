db22c4ef053fb2b74bc99c40ac8ff983a6f8f1ac
mrdjurayev <mrabudjurayev@gmail.com>
2025-06-14 20:16:00 +0500

    Exercise 99 solved

--- Exercises-javascript/script.js
+++ Exercises-javascript/script.js
@@ -1,27 +1,27 @@
-// 98. Write a Javascript program to change the case of the minimum number of letters to make a given string written in upper case or lower case.
-// For example: "Write" will be write and "PHp" will be "PHP".
+// 99. Write a JavaScript program to check whether it is possible to rearrange the characters of a given string. 
+// This is in such a way that it will become equal to another given string. 
 
-function toUpperOrLowerCase(str) {
-    
-    let upperChars = 0;
-    let lowerChars = 0;
+function canRearrangeToMatch(str1, str2) {
+    if (str1.length !== str2.length) return false;
 
-    for(let i = 0; i < str.length; i++) {
-        let char = str[i];
+    function countLetters(str) {
+        let map = {};
+        for (let letter of str) {
+            map[letter] = (map[letter] || 0) + 1;
+        }
+        return map;
+    }
 
-        if(/[A-Z]/.test(char)) {
-            upperChars++;
-        } else if(/[a-z]/.test(char)) {
-            lowerChars++;
+    let str1Letters = countLetters(str1);
+    let str2Letters = countLetters(str2);
+
+    for (let char in str1Letters) {
+        if (str1Letters[char] !== str2Letters[char]) {
+            return false;
         }
     }
 
-    if(upperChars > lowerChars) {
-        return str.toUpperCase();
-    } else if(upperChars < lowerChars) {
-        return str.toLowerCase();
-    }
-    else return str;
+    return true;
 }
 
-console.log(toUpperOrLowerCase('HeLLo'));
+console.log(canRearrangeToMatch('hello', 'olleh'));
